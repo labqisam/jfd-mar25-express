322574d0c8f7b4e3d44594d1bbf8f4c876c4b02c
muhiqbalibrahim <muhiqbalibrahim962@gmail.com>
2025-05-07 20:32:09 +0800

    install mysql2

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-let nama = "Iqbal"
-let tempat = "Course-Net"
-console.log(`Selamat Datang ${nama} di ${tempat}`);
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'view-ejs');
+
+app.get('/', (req, res) => {
+    let data_untuk_view = {
+        nama: 'Iqbal'
+    }
+    res.render('beranda', data_untuk_view)
+})
+
+app.listen(3000, () => {
+    console.log('Server sudah on, silahkan ke http://localhost:3000');    
+})
