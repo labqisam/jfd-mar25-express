5cefc107474272f01ca0ff0a4162310b07abd8b8
muhiqbalibrahim <muhiqbalibrahim962@gmail.com>
2025-05-08 10:01:34 +0800

    koneksi ke database dan fetching data

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jfd_maret_2025'
+})
+
+db.connect(function(){
+    console.log('Berhasil terhubung ke mysql\n======');
+})
+
+db.addListener('error', function(err){
+    console.log(err);    
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'view-ejs');
@@ -10,6 +26,24 @@ app.get('/', (req, res) => {
     }
     res.render('beranda', data_untuk_view)
 })
+
+app.get('/karyawan', async (req, res) => {
+    let dataKaryawan = new Promise((resolve, reject) => {
+        db.query('SELECT * FROM karyawan', (errorSQL, dataSQL) => {
+            if(errorSQL){
+                reject(errorSQL)
+            } else {
+                resolve(dataSQL)
+            }
+        })
+    })
+
+    let dataView = {
+        dakar: await dataKaryawan
+    }
+
+    res.render('karyawan/index', dataView);
+});
 
 app.listen(3000, () => {
     console.log('Server sudah on, silahkan ke http://localhost:3000');    
